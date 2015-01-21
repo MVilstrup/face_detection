6dbbc622b39b2a3846ddaa277c744f78964fc3cf
Mikkel Vilstrup <mikkel@vilstrup.dk>
2015-01-21 16:59:06 +0100

    Created the foundation

---
+++ README.md
@@ -0,0 +1,10 @@
+This is a simple repository for getting started working with computer vision easily. 
+
+Note: 
+if you are working with mac you will need to install:
+
+VLC:
+
+
+XQUARTZ:
+http://xquartz.macosforge.org/landing/
---
+++ set_up_vision_environment.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+# Setup the system and install the most necessary libraries
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install gcc g++ gettext unzip make cmake checkinstall libicu48 -y
+
+# Install OpenCV
+version="2.4.9"
+echo "Installing OpenCV" $version
+mkdir OpenCV
+cd OpenCV
+echo "Removing any pre-installed ffmpeg and x264"
+sudo apt-get -qq remove ffmpeg x264 libx264-dev
+echo "Installing Dependenices"
+sudo apt-get install build-essential libgtk2.0-dev libjpeg-dev libtiff4-dev libjasper-dev libopenexr-dev cmake python-dev python-numpy python-tk libtbb-dev libeigen3-dev yasm libfaac-dev libopencore-amrnb-dev libopencore-amrwb-dev libtheora-dev libvorbis-dev libxvidcore-dev libx264-dev libqt4-dev libqt4-opengl-dev sphinx-common texlive-latex-extra libv4l-dev libdc1394-22-dev libavcodec-dev libavformat-dev libswscale-dev default-jdk ant libvtk5-qt4-dev -y
+echo "Downloading OpenCV" $version
+wget -O OpenCV-$version.zip http://sourceforge.net/projects/opencvlibrary/files/opencv-unix/$version/opencv-"$version".zip/download
+echo "Installing OpenCV" $version
+sudo unzip OpenCV-$version.zip
+cd opencv-$version
+sudo mkdir build
+cd build
+sudo cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_TBB=ON -D BUILD_NEW_PYTHON_SUPPORT=ON -D WITH_V4L=ON -D INSTALL_C_EXAMPLES=ON -D INSTALL_PYTHON_EXAMPLES=ON -D BUILD_EXAMPLES=ON -D WITH_QT=ON -D WITH_OPENGL=ON ..
+sudo make -j2
+sudo checkinstall
+sudo sh -c 'echo "/usr/local/lib" > /etc/ld.so.conf.d/opencv.conf'
+sudo ldconfig
+sudo sh -c 'echo "PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/usr/local/lib/pkgconfig \n export PKG_CONFIG_PATH" > /etc/bash.bashrc'
+
+echo "OpenCV" $version "ready to be used"
+
+sudo apt-get gedit -y
+
+# Install the final libraries for working with images in python 
+sudo apt-get install python-pip python-scipy -y
+pip install mahotas
+
+# Make sure you can open images and videos on your local computer
+sudo apt-get install vlc -y
+
