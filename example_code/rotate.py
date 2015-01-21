8d4402e230a4dbe76e1266bf8e15822864df89fa
Mikkel Vilstrup <mikkel@vilstrup.dk>
2015-01-21 19:42:41 +0100

    Created snippet showing how to rotate images in OpenCV along with utils for easy rotation

---
+++ example_code/rotate.py
@@ -0,0 +1,27 @@
+import numpy as np
+import argparse
+from utils import imutils
+import cv2
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required = True, help="Path to the image")
+args = vars(ap.parse_args())
+
+image = cv2.imread(args["image"])
+cv2.imshow("Original", image)
+
+# Showcase of the manual way of rotation an image
+
+(height, width) = image.shape[:2] # get the two first values of the image which is height and width
+center = (width / 2, height / 2)
+
+matrix = cv2.getRotationMatrix2D(center, 45, 1.0)
+rotated = cv2.warpAffine(image, matrix, (width, height))
+
+cv2.imshow("Rotated by 45 Degrees", rotated)
+
+# Showcase of rotating an image using the defined utils function rotate
+rotated = imutils.rotate(image, 180)
+cv2.imshow("Rotated by 180 Degrees", rotated)
+
+cv2.waitKey(0)
--- example_code/utils/imutils.py
+++ example_code/utils/imutils.py
@@ -5,3 +5,14 @@ def translate(image, x, y):
     matrix = np.float32([[1, 0, x], [0, 1, y]])
     shifted = cv2.warpAffine(image, matrix, (image.shape[1], image.shape[0]))
     return shifted
+
+def rotate(image, angle, center = None, scale = 1.0):
+    (height, width) = image.shape[:2]
+
+    if center is None:
+        center = (width / 2, height / 2)
+
+    matrix = cv2.getRotationMatrix2D(center, angle, scale)
+    rotated = cv2.warpAffine(image, matrix, (width, height))
+
+    return rotated
