966b47fca9a53d6d98ee8cee5c20ef846dd04268
Mikkel Vilstrup <mikkel@vilstrup.dk>
2015-01-22 12:10:11 +0100

    Added two new examples

---
+++ example_code/cropping.py
@@ -0,0 +1,17 @@
+# Cropping in OpenCV simply utilizes array slicing in Numpy.
+
+import argparse
+import cv2
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True, help="Path to the image")
+args = vars(ap.parse_args())
+
+image = cv2.imread(args["image"])
+cv2.imshow("Original", image)
+
+# cropping an image manually
+cropped = image[30:120, 240:335] # If used on the image trex.png this encapsulates its head
+cv2.imshow("Cropped image", cropped)
+
+cv2.waitKey(0)
