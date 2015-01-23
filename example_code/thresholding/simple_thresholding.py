39ccf0f60b06f7ad284337a4cb0fc731f167ec8f
Mikkel Vilstrup <mikkel@vilstrup.dk>
2015-01-23 10:41:26 +0100

    Added example for adaptive thresholding

--- example_code/thresholding/simple_thresholding.py
+++ example_code/thresholding/simple_thresholding.py
@@ -12,6 +12,8 @@ image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 blurred = cv2.GaussianBlur(image, (5, 5), 0)
 cv2.imshow("Image", image)
 
+# This way of doing thresholding is not optimal since threshvalue has to be defined by the developer (in this case 155)
+# This code works on the coins.png image to select the coins and not the table
 (T, thresh) = cv2.threshold(blurred, 155, 255, cv2.THRESH_BINARY)
 cv2.imshow("Threshold Binary", thresh)
 
